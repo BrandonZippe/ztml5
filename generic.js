c70475f1e3b277d1a53c3cfc2e13edbf66c59555
BrandonZippe <btzippe@gmail.com>
2014-03-23 23:31:11 -0500

    Site Update
    Created homepage setTimeout action (home.js);
    added music content to “lstn” page;
    added meta data and analytics;
    updates <head> on all pages;
    styles.css updated;

--- generic.js
+++ generic.js
@@ -1,32 +1,32 @@
+$('.navContent').hide();
+
+$('.navTab').click(function (event) {
+	event.preventDefault();
+	$('.navTab').removeClass('displayed');
+	$(this).addClass('displayed');
+	$('.navTab').hide();
+	
+	var selectedTabIndex = $('.navSet .displayed').index();
 	$('.navContent').hide();
-	
-	$('.navTab').click(function (event) {
-		event.preventDefault();
-		$('.navTab').removeClass('displayed');
-		$(this).addClass('displayed');
-		$('.navTab').hide();
-		
-		var selectedTabIndex = $('.navSet .displayed').index();
-		$('.navContent').hide();
-		$('.navContent').eq(selectedTabIndex).show();
-	});
-	
-	$('.closeMod').click(function () {
-		$('.navContent').hide();
-		$('.navTab').show();
-	});
+	$('.navContent').eq(selectedTabIndex).show();
+});
+
+$('.closeMod').click(function () {
+	$('.navContent').hide();
+	$('.navTab').show();
+});
 
 
-	$('.cms_tab_content').hide();
-	$('.cms_tab_content').first().show();
-	$('.cms_tabset_btn').first().addClass('cms_selected_tab');
-	
-	$('.cms_tabset_btn').click(function (event) {
-        event.preventDefault();
-        $('.cms_tabset_btn').removeClass('cms_selected_tab');
-        $(this).addClass('cms_selected_tab');
+$('.cms_tab_content').hide();
+$('.cms_tab_content').first().show();
+$('.cms_tabset_btn').first().addClass('cms_selected_tab');
 
-        var selectedTAbIndex = $('.cms_tabset .cms_selected_tab').index();
-        $('.cms_tab_content').hide();
-        $('.cms_tab_content').eq(selectedTAbIndex).show();
-    });
+$('.cms_tabset_btn').click(function (event) {
+    event.preventDefault();
+    $('.cms_tabset_btn').removeClass('cms_selected_tab');
+    $(this).addClass('cms_selected_tab');
+
+    var selectedTAbIndex = $('.cms_tabset .cms_selected_tab').index();
+    $('.cms_tab_content').hide();
+    $('.cms_tab_content').eq(selectedTAbIndex).show();
+});
---
+++ home.js
@@ -0,0 +1,3 @@
+setTimeout(function(){
+	$('.mastHead').addClass('slide');
+	$('.backgroundHome').addClass('primary');},5000);
